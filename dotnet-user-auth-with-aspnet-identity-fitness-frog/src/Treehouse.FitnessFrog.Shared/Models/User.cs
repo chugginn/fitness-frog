a88b9e46584a1a5a0a713961a14fdbba59e08071
Mark Ramos <markramos83@gmail.com>
2018-12-04 10:55:33 -0700

    Added User relationship to the Entry class.

--- dotnet-user-auth-with-aspnet-identity-fitness-frog/src/Treehouse.FitnessFrog.Shared/Models/User.cs
+++ dotnet-user-auth-with-aspnet-identity-fitness-frog/src/Treehouse.FitnessFrog.Shared/Models/User.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Treehouse.FitnessFrog.Shared.Models
 {
@@ -12,13 +6,5 @@ namespace Treehouse.FitnessFrog.Shared.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<User>
-    {
-        public ApplicationDbContext()
-            : base("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FitnessFrog;Integrated Security=True;MultipleActiveResultSets=True")
-        {
-        }
     }
 }
